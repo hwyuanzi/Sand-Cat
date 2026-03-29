f4ef0f9123c19798ba2c78254779cff8a27890d2
TuoZhang0902 <tuozhang0902@gmail.com>
2026-03-29 17:32:05 -0400

    update demo, pyproject, and package structure

--- demo.py
+++ demo.py
@@ -2,10 +2,9 @@ from sandcat_fun import (
     cat_greeting,
     cat_comfort,
     cat_fortune,
+    draw_cat,
+    get_fortune,
 )
-from draw import draw_cat
-from fortune import get_fortune
-
 
 def main():
     print("=== Sand Cat Package Demo ===\n")
